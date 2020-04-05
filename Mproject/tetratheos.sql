b4d9c9d75aecbcc879347a2a11c2ecb357a58da2
Karuto01 <yongjl01@hotmail.com>
2020-04-05 14:15:21 +0800

    Update tetratheos.sql

--- Mproject/tetratheos.sql
+++ Mproject/tetratheos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2020 at 05:44 PM
+-- Generation Time: Apr 04, 2020 at 08:49 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -21,9 +21,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `tetratheos`
 --
-drop database tetratheos;
-create database tetratheos;
+
 use tetratheos;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+
+CREATE TABLE `announcement` (
+  `announcementInfo` varchar(300) DEFAULT NULL,
+  `announcementNote` varchar(300) DEFAULT NULL,
+  `adminID` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `announcement`
+--
+
+INSERT INTO `announcement` (`announcementInfo`, `announcementNote`, `adminID`) VALUES
+('Today no class', '-Remind student to wear underwear', 'A101');
+
 -- --------------------------------------------------------
 
 --
@@ -42,18 +60,48 @@ CREATE TABLE `assignment` (
 --
 
 INSERT INTO `assignment` (`assignmentID`, `dueDate`, `assignmentType`, `subjectID`) VALUES
-('DIP1CTS03a', '2020-03-28 23:58:22', 'a', 'DIP1CTS03'),
+('DIP1CTS03a', '2020-03-12 14:00:00', 'a', 'DIP1CTS03'),
 ('DIP1CTS03b', '2020-03-28 23:58:29', 'b', 'DIP1CTS03'),
 ('DIP1ITC04a', '2020-03-28 23:58:36', 'a', 'DIP1ITC04'),
-('DIP1MPR01a', '2020-03-28 23:58:54', 'a', 'DIP1MPR01'),
+('DIP1MPR01a', '2020-04-06 14:00:00', 'a', 'DIP1MPR01'),
 ('DIP1PRG12a', '2020-03-28 23:59:16', 'a', 'DIP1PRG12'),
 ('DIP200a', '2020-03-28 23:59:23', 'a', 'DIP200'),
 ('DIP202a', '2020-03-28 23:59:33', 'a', 'DIP202'),
 ('DIP202b', '2020-03-28 23:59:38', 'b', 'DIP202'),
 ('DIP203a', '2020-03-28 23:59:51', 'a', 'DIP203'),
 ('DIP203b', '2020-03-28 23:59:55', 'b', 'DIP203'),
+('DIP216', '2020-04-03 17:06:36', '-', 'DIP216'),
 ('DIP222a', '2020-03-29 00:00:09', 'a', 'DIP222'),
 ('DIP222b', '2020-03-29 00:00:13', 'b', 'DIP222');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignmentmark`
+--
+
+CREATE TABLE `assignmentmark` (
+  `studentID` varchar(20) NOT NULL,
+  `assignmentID` varchar(20) NOT NULL,
+  `mark` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assignmentmark`
+--
+
+INSERT INTO `assignmentmark` (`studentID`, `assignmentID`, `mark`) VALUES
+('B1900071', 'DIP1MPR01a', NULL),
+('B1900071', 'DIP1PRG12a', NULL),
+('B1900071', 'DIP202a', NULL),
+('B1900071', 'DIP202b', NULL),
+('B1900071', 'DIP203a', NULL),
+('B1900071', 'DIP203b', NULL),
+('B1900071', 'DIP222a', NULL),
+('B1900071', 'DIP222b', NULL),
+('B1900095', 'DIP1MPR01a', NULL),
+('B1900095', 'DIP202a', NULL),
+('B1900095', 'DIP202b', NULL);
 
 -- --------------------------------------------------------
 
@@ -63,22 +111,22 @@ INSERT INTO `assignment` (`assignmentID`, `dueDate`, `assignmentType`, `subjectI
 
 CREATE TABLE `enrollment` (
   `subjectID` varchar(20) NOT NULL,
-  `studentID` varchar(20) NOT NULL,
-  `mark` int(10) DEFAULT NULL
+  `studentID` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `enrollment`
 --
 
-INSERT INTO `enrollment` (`subjectID`, `studentID`, `mark`) VALUES
-('DIP1MPR01', 'B1900071', NULL),
-('DIP1MPR01', 'B1900095', NULL),
-('DIP1PRG12', 'B1900071', NULL),
-('DIP202', 'B1900071', NULL),
-('DIP202', 'B1900095', NULL),
-('DIP203', 'B1900071', NULL),
-('DIP222', 'B1900071', NULL);
+INSERT INTO `enrollment` (`subjectID`, `studentID`) VALUES
+('DIP1ITC04', 'B1800931'),
+('DIP1MPR01', 'B1900071'),
+('DIP1MPR01', 'B1900095'),
+('DIP1PRG12', 'B1900071'),
+('DIP202', 'B1900071'),
+('DIP202', 'B1900095'),
+('DIP203', 'B1900071'),
+('DIP222', 'B1900071');
 
 -- --------------------------------------------------------
 
@@ -89,23 +137,25 @@ INSERT INTO `enrollment` (`subjectID`, `studentID`, `mark`) VALUES
 CREATE TABLE `subject` (
   `subjectID` varchar(20) NOT NULL,
   `subjectName` varchar(50) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `lectureID` varchar(20) NOT NULL
+  `enrollmentKey` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `lecturerID` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`subjectID`, `subjectName`, `description`, `lectureID`) VALUES
-('DIP1CTS03', 'Computer Technology Essentials', 'Com tech', 'L102'),
-('DIP1ITC04', 'Introduction to Networking', 'Networking', 'L101'),
-('DIP1MPR01', 'IT Mini Project', 'final year killer', 'L101'),
-('DIP1PRG12', 'Introduction to Structured Programming', 'C programming', 'L103'),
-('DIP200', 'Introduction to Computer Architecture', 'Com Arch', 'L105'),
-('DIP202', 'Business Communication', 'BUSINESS OF ALL COMMUNICATION TO TALK COMM', 'L104'),
-('DIP203', 'Database Concepts and Practices', 'DATABASE LANAT\r\n', 'L105'),
-('DIP222', 'Python', 'FONG DA BEST LECUTRER U SEE NOTHING', 'L104');
+INSERT INTO `subject` (`subjectID`, `subjectName`, `enrollmentKey`, `password`, `lecturerID`) VALUES
+('DIP1CTS03', 'Computer Technology Essentials', 'comtech', 'abc', 'L102'),
+('DIP1ITC04', 'Introduction to Networking', '', 'abc', 'L101'),
+('DIP1MPR01', 'IT Mini Project', '', 'abc', 'L101'),
+('DIP1PRG12', 'Introduction to Structured Programming', 'iseeuc', 'abc', 'L103'),
+('DIP200', 'Introduction to Computer Architecture', 'comarch', 'abc', 'L105'),
+('DIP202', 'Business Communication', 'busicom', 'abc', 'L104'),
+('DIP203', 'Database Concepts and Practices', 'datadata', 'abc', 'L105'),
+('DIP216', 'Industrial Internship', 'internintern', 'abc', 'L102'),
+('DIP222', 'Python', 'paithon', 'abc', 'L104');
 
 -- --------------------------------------------------------
 
@@ -116,11 +166,20 @@ INSERT INTO `subject` (`subjectID`, `subjectName`, `description`, `lectureID`) V
 CREATE TABLE `submission` (
   `submissionID` varchar(20) NOT NULL,
   `submissionDateTime` datetime NOT NULL DEFAULT current_timestamp(),
-  `adminID` varchar(20) NOT NULL,
+  `submissionFile` longblob NOT NULL,
   `studentID` varchar(20) NOT NULL,
   `subjectID` varchar(20) NOT NULL,
   `assignmentID` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `submission`
+--
+
+INSERT INTO `submission` (`submissionID`, `submissionDateTime`, `submissionFile`, `studentID`, `subjectID`, `assignmentID`) VALUES
+('SBMDIP1MPR01B1900071', '2020-04-04 14:12:11', 0x43565f4a482e646f6378, 'B1900071', 'DIP1MPR01', 'DIP1MPR01a'),
+('SBMDIP1PRG12B1900071', '2020-04-05 02:15:16', 0x636172645f6e616d655f646174652e706466, 'B1900071', 'DIP1PRG12', 'DIP1PRG12a'),
+('SBMDIP222B1900071', '2020-04-05 01:59:03', 0x6c65747465725f6e616d655f646174652e706466, 'B1900071', 'DIP222', 'DIP222a');
 
 -- --------------------------------------------------------
 
@@ -147,10 +206,10 @@ INSERT INTO `user` (`userID`, `name`, `phoneNumber`, `email`, `address`, `userTy
 ('B1800931', 'Kai Sen', '0122308273', 'kaisen321@hotmail.com', '', 's', 'abc'),
 ('B1900071', 'Johann', '0122309882', 'choojiahan@gmail.com', '', 's', 'abc'),
 ('B1900095', 'Jia Le', '01237483432', 'yugidragon@gmail.com', '', 's', 'abc'),
-('L101', 'Steve', '0139382732', 'steve69@yahoo.com', '', 'l', 'abc'),
+('L101', 'Steve', '012309283', 'steve321@gmail.com', '', 'l', 'abc'),
 ('L102', 'Koo', '0139283924', 'koo321@gmail.com', '', 'l', 'abc'),
 ('L103', 'Seeth', '0189283923', 'seeth11@gmail.com', '', 'l', 'abc'),
-('L104', 'Fung', '0138327883', 'fung32@gmail.com', '', 'l', 'abc'),
+('L104', 'Fung', '0133293723', 'fung32@gmail.com', '', 'l', 'abc'),
 ('L105', 'Dew', '0134928392', 'dew0000@gmail.com', '', 'l', 'abc');
 
 --
@@ -158,10 +217,22 @@ INSERT INTO `user` (`userID`, `name`, `phoneNumber`, `email`, `address`, `userTy
 --
 
 --
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`adminID`);
+
+--
 -- Indexes for table `assignment`
 --
 ALTER TABLE `assignment`
   ADD PRIMARY KEY (`assignmentID`);
+
+--
+-- Indexes for table `assignmentmark`
+--
+ALTER TABLE `assignmentmark`
+  ADD PRIMARY KEY (`studentID`,`assignmentID`);
 
 --
 -- Indexes for table `enrollment`
